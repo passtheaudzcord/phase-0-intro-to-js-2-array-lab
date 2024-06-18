2ac3071000e69abbad788c81bbf3f455925a9d8e
Audrey <chipmanaudrey@gmail.com>
2024-06-18 14:41:04 -0700

    Practice with array methods

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+ function destructivelyAppendCat(Ralph) {
+ cats.push("Ralph");
+ }
+
+ function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob");
+ }
+
+ function destructivelyRemoveLastCat(Garfield) {
+cats.pop();
+ }
+
+ function destructivelyRemoveFirstCat(Milo) {
+cats.shift();
+ }
+
+ function appendCat(Broom) {
+     return [...cats, "Broom"];
+ }
+
+function prependCat(Arnold) {
+   return ["Arnold", ...cats];
+ }
+
+ function removeLastCat() {
+    return cats.slice(0, -1);
+ }
+
+ function removeFirstCat() {
+    return cats.slice(1);
+ }
